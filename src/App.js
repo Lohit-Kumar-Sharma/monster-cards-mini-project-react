39d0a57b31edd0901cdf0db2e5f07d6827b259ed
Lohit-Kumar-Sharma <32270035+Lohit-Kumar-Sharma@users.noreply.github.com>
2020-03-18 19:08:33 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {SearchBox} from './components/search-box/search-box.component';
+import {CardLista} from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+       monsters :   [],
+       searchText : ''
+    };
+  }
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}));
+  }
+  render()
+  { 
+    const { monsters, searchText} = this.state;
+    const filteredmonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+      console.log(""+monsters+" "+searchText);
+    return(
+          <div className='App'>
+          <h1>#100daysofcode Challange</h1>
+          <h2>Day [9-100]</h2>
+          <h2>Learning React</h2>
+          <h2>@coding_aasan_hai</h2>
+          
+          <SearchBox placeholder ='Search Monsters' handleChange={e => this.setState({searchText : e.target.value})} />
+          <CardLista monsters = {filteredmonsters}>
+           
+          </CardLista>
+                     
+          </div>
+       
+    );
+    
+  }
 }
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange}) =>(
+    <div ><input className='search' type='search' placeholder={placeholder} onChange={handleChange}></input>
+</div>
+);
