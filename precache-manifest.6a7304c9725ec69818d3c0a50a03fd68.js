9ff6739eef91a76924bbbb594882654534e58690
Lohit-Kumar-Sharma <32270035+Lohit-Kumar-Sharma@users.noreply.github.com>
2020-03-18 18:56:01 +0530

    Updates

---
+++ precache-manifest.6a7304c9725ec69818d3c0a50a03fd68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254621bda86c33126f0c3701d9e265f3",
+    "url": "/monster-cards-mini-project-react/index.html"
+  },
+  {
+    "revision": "f76821bfe5bf0b3c89ce",
+    "url": "/monster-cards-mini-project-react/static/css/main.a35e5ca4.chunk.css"
+  },
+  {
+    "revision": "8f635bfd5ab1c0519b19",
+    "url": "/monster-cards-mini-project-react/static/js/2.4531020a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-cards-mini-project-react/static/js/2.4531020a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76821bfe5bf0b3c89ce",
+    "url": "/monster-cards-mini-project-react/static/js/main.052082b6.chunk.js"
+  },
+  {
+    "revision": "4e8573c41c7b00a15e48",
+    "url": "/monster-cards-mini-project-react/static/js/runtime-main.2daf6f94.js"
+  }
+]);
